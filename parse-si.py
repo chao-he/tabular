8a8c5a1ee44a75fa6a6faad968e92337ffc48506
hechao <hechaos@gmail.com>
2023-09-11 14:59:17 +0800

    update

--- parse-si.py
+++ parse-si.py
@@ -4,11 +4,6 @@ import pandas as pd
 from glob import glob
 
 def read_data(f):
-    try:
-        return pd.read_excel(f)
-    except ValueError as e:
-        pass
-
     for encoding in ["utf-8", "latin-1"]:
         try:
             return pd.read_csv(f, on_bad_lines='skip', encoding=encoding, sep='[,;\t]', engine='python')
@@ -18,24 +13,24 @@ def read_data(f):
             pass
     return None
 
-valid_sname_pat = re.compile('smi|struct', re.I)
+valid_sname_pat = re.compile('smile|struct', re.I)
 
-slist = []
-for pdf in glob("./tables/*"):
-    compounds = {}
-    doi = os.path.basename(pdf)
-    for si in sorted(glob(f"../dev/tabular/si/10.1021/{doi}*.csv")):
-        try:
-            df = read_data(si)
-            cname = df.columns[0]
-            for sname in df.columns:
-                if valid_sname_pat.findall(sname):
-                    for cid, smiles in zip(df[cname], df[sname]):
-                        compounds[cid.strip()] = smiles.strip()
-        except Exception as e:
-            pass
-    if compounds:
-        with open(f"./tables/{doi}/compounds.txt", "w") as output:
-            for cid, smiles in compounds.items():
-                output.write(f"{cid}\t{smiles}\n")
-    print(doi, len(compounds))
+
+db = {}
+
+for sp in sorted(glob(f"./notebook/data/acs.jmedchem.*.csv")):
+    doi = os.path.basename(sp)[:-21]
+    db[doi] = {}
+
+for sp in sorted(glob(f"./notebook/data/acs.jmedchem.*.csv")):
+    doi = os.path.basename(sp)[:-21]
+    df = read_data(sp)
+    cname = df.columns[0]
+    sname = [c for c in df.columns if valid_sname_pat.findall(c)]
+    if sname:
+        print(cname)
+
+    continue
+
+    for cid, smiles in zip(df[cname], df[sname]):
+        db[doi][cid.strip()] = smiles.strip()
