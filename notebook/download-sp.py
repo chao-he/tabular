d1d50e450b67fa4cb853c8de1dc2b643604bcac0
hechao <hechaos@gmail.com>
2023-09-15 11:33:42 +0800

    bugfix

--- notebook/download-sp.py
+++ notebook/download-sp.py
@@ -1,28 +1,43 @@
 import os
-import pandas as pd
 import requests
+import pandas as pd
 from glob import glob
 from multiprocessing import Pool
 
+
 def fetch_sp(doi):
-    baseurl = 'https://widgets.figshare.com/public/files?institution=acs&limit=21&offset=0&collectionResourceDOI='
-    r = requests.get(f"{baseurl}10.1021/{doi}")
+    url = f"https://widgets.figshare.com/public/files?institution=acs&limit=21&offset=0&collectionResourceDOI={doi}"
+    r = requests.get(url)
     assert r.status_code == 200
+    print(f"download {doi} -> {r.status_code}")
     for f in r.json()["files"]:
-        name, durl = f["name"], f["downloadUrl"]
-        _, ext = os.path.splitext(name)
-        if ext in ['.pdf', '.pdb'] or os.path.exists(f"data/{doi}.{name}"):
+        fname, durl = f["name"], f["downloadUrl"]
+        _, ext = os.path.splitext(fname)
+        if ext in ['.xls', '.xlsx', '.zip', '.csv'] and not os.path.exists(f"data/{doi}.{fname}"):
+            r = requests.get(durl)
+            assert r.status_code == 200
+            with open(f"data/{doi}.{fname}", "wb") as output:
+                output.write(r.content)
+            print(doi, fname, durl, ext)
+
+
+if __name__ == "__main__":
+    df = pd.read_csv("./jmc-article-stat.txt", sep='\t', header=None,
+                     names=["doi", "y", "h", "w", "subtype", "venue"])
+
+    cache = set()
+    for f in glob("data/10.1021/*"):
+        doi, ext = os.path.splitext(os.path.basename(f))
+        doi = ".".join(doi.split(".")[:-1])
+        cache.add(doi)
+
+    seeds = []
+    for _,row in df.iterrows():
+        if doi in cache:
             continue
-        r = requests.get(durl)
-        assert r.status_code == 200
-        with open(f"data/{doi}.{name}", "wb") as output:
-            output.write(r.content)
-        print(doi, name, durl, ext)
+        doi = "10.1021/" + row["doi"]
+        seeds.append(doi)
 
-
-df = pd.read_csv("jmc-article.txt", sep='\t', header=None, names=["doi", "y", "h", "w", "subtype", "venue"])
-for doi in df.doi:
-    if not list(glob(f"./data/{doi}*.csv")):
-        print(doi)
-        fetch_sp(doi)
-
+    print(len(df), len(seeds), len(cache))
+    with Pool(10) as p:
+        p.map(fetch_sp, seeds)
--- notebook/parse-si.py
+++ notebook/parse-si.py
@@ -1,70 +1,122 @@
 import os
+import sys
 import re
 import pandas as pd
-from pandas.errors import EmptyDataError
+import numpy as np
 import rdkit.Chem as Chem
+from collections import defaultdict
+from pandas.errors import EmptyDataError
+from molvs import standardize_smiles
 from glob import glob
 
-def read_data(f, sep=None):
+
+def detect_line_sep(fn, enc='latin-1'):
+    lines = open(fn, encoding=enc).read().splitlines()
+    c = [np.random.randint(len(lines)) for i in range(min(8, len(lines)))]
+    for ix in [0] + c:
+        line = lines[ix].strip()
+        a, b = line.count(","), line.count(";")
+        if a == 0 or b == 0:
+            break
+    if a == 0 and b > 0:
+        return ";"
+    elif a > 0 and b == 0:
+        return ","
+    else:
+        return "[,;]"
+
+
+def like_smiles(s):
+    smiles_pat = re.compile(r'([CHONPSFI]|Cl|Br|[1-9]|[=#@\/]|[\x28\x29\x5b\x5d]){5,}', re.I)
+    if isinstance(s, str) and len(s) > 3:
+        token = 0
+        for c in s.upper():
+            if c in ['C', 'H', 'O', 'N', 'P', 'S', 'F', 'I', '=', '#']:
+                token += 1
+        if token > 8:
+            return True
+
+        m = smiles_pat.match(s)
+        if m and m[0].count('C')>3:
+            return True
+    return False
+
+def find_smiles_column(df):
+    for word in ["smile", "simile", "structure", "molecular", "formula", "string"]:
+        for j, column in enumerate(df.columns):
+            if column.lower().find(word) >= 0:
+                return j
+    for i, row in df.iterrows():
+        for j, s in enumerate(row):
+            if like_smiles(s):
+                return j
+    return -1
+
+
+def detect_csv_format(f):
+    sep = detect_line_sep(f, 'latin-1')
+
+    skip = 0
+    for i, line in enumerate(open(f, encoding='latin-1')):
+        line = line.lower()
+        is_head = False
+        for word in ["compound", "smiles"]:
+            if line.find(word) >= 0:
+                is_head = True
+                break
+        if is_head:
+            skip = i
+
+    df = pd.read_csv(f, encoding='latin-1', sep=sep, skiprows=skip, on_bad_lines='skip', engine='python')
+
+    smiles_row_ix, smiles_col_ix = skip + 1, find_smiles_column(df)
+
+    if smiles_col_ix >= 0:
+        sname = df.columns[smiles_col_ix]
+        for i, s in enumerate(df[sname]):
+            if like_smiles(s):
+                smiles_row_ix = skip + i + 1
+                break
+
+    content = open(f, encoding='latin-1').read().splitlines()
+    columns = ['' for j in range(len(df.columns))]
+
+    for i in range(smiles_row_ix):
+        t = content[i].split(sep)
+        for j in range(len(columns)):
+            if j < len(t) and t[j]:
+                columns[j] += " " + t[j]
+    columns = [c.strip() for c in columns]
+    return 'latin-1', sep, smiles_row_ix, smiles_col_ix, columns
+
+
+def read_data(f):
     _, ext = os.path.splitext(f)
+
+    magic = open(f, 'rb').read(4)
+    if magic == b'PK\x03\x04' and ext == ".csv":
+        ext = ".xlsx"
+
     if ext in [".xls", ".xlsx"]:
         return pd.read_excel(f)
-    elif ext in [".csv"]:
-        lines = open(f, encoding='latin-1').read().splitlines()
+
+    elif ext == ".csv":
         try:
-            return pd.read_csv(f, encoding='latin-1', on_bad_lines='skip', sep=sep, engine='python')
-        except EmptyDataError as e:
-            pass
-    return None
+            enc, sep, smiles_row_ix, smiles_col_ix, columns = detect_csv_format(fn)
+            # print(fn, smiles_row_ix, smiles_col_ix, columns)
+            df = pd.read_csv(f, encoding=enc, sep=sep, skiprows=smiles_row_ix, on_bad_lines='skip', engine='python')
+            df.columns = (columns + ['Unamed: {i}' for i in range(len(df.columns) - len(columns))])[:len(df.columns)]
+            return df
+        except pd.errors.EmptyDataError:
+            return None
 
-valid_sname_pat = re.compile('smile|struct', re.I)
 
-files = list(glob(f"data/*.csv")) + list(glob("data/*.xls*"))
-
-db = {}
-for sp in sorted(files):
-    fname, ext = os.path.splitext(os.path.basename(sp))
-    doi = ".".join(fname.split(".")[:-1])
-
-    compounds = db.get(doi, {})
-
-    ss = [",", ";"]
-
-    lines = open(sp, encoding='latin-1').read().splitlines()
-    if len(lines) > 2:
-        a, b = len(lines[2].split(";")), len(lines[1].split(";"))
-        if a == b and a > 2:
-            ss = [";", ","]
-
-    for sep in ss:
-        sdf = read_data(sp, sep)
-        if sdf is None:
+if __name__ == "__main__":
+    root_dir = "../jmc-article-discovery-formal-table"
+    doc_list = [os.path.basename(fn)[:-4] for fn in glob(f"{root_dir}/*.pdf")]
+    for fn in glob("./data/10.1021/*.*"):
+        df = read_data(fn)
+        if df is None:
             continue
-        cname = sdf.columns[0]
-        sname = [c for c in sdf.columns if valid_sname_pat.findall(c.lower())]
-        if len(sname) > 0:
-            scol = sname[0]
-            for x in sname:
-                if x.lower().find("smi") >= 0:
-                    scol = x
-            sdf = sdf[[cname, scol]].dropna()
-            for cid, smiles in zip(sdf[cname], sdf[scol]):
-                if isinstance(smiles, float):
-                    continue
-                if isinstance(cid, float) or isinstance(cid, int):
-                    cid = str(round(cid))
-                cid = " ".join(cid.split())
-                smiles = str(smiles).strip().strip(";").strip(",")
-                mol = Chem.MolFromSmiles(smiles)
-                if mol:
-                    compounds[cid] = smiles
-        if compounds:
-            break
-
-    if compounds:
-        db[doi] = compounds
-
-
-for doi, compounds in db.items():
-    for cid, smiles in compounds.items():
-        print(f"{doi}\t{cid}\t{smiles}")
+        ix = find_smiles_column(df)
+        print(fn, df.columns[ix])
