1e67ac43ce1b1d93739e1cb7e856cd37debb3765
hechao <hechaos@gmail.com>
2023-09-18 15:13:38 +0800

    add p01-table2png.py

---
+++ p01-table2png.py
@@ -0,0 +1,67 @@
+import os
+import re
+import json
+import fitz
+import cv2
+import numpy as np
+from glob import glob
+
+
+def get_image(page, direction, bbox, dpi=256):
+    pix = doc[pn].get_pixmap(clip=bbox, dpi=dpi, matrix=fitz.Matrix(2,2))
+    img = np.frombuffer(buffer=pix.samples_mv, dtype=np.uint8)\
+        .reshape((pix.height, pix.width, -1))
+
+    unit = img.shape[1]/bbox.width
+
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lo, hi = np.array([156,  43,  46]), np.array([179, 255, 255])
+    mask = cv2.inRange(hsv, lo, hi)
+    mask[mask>0] = 1
+    for i in range(60, mask.shape[0]):
+        l = np.sum(mask[i,:])
+        if l > min(800, img.shape[1]*0.75):
+            img = img[:i,:,:]
+            break
+
+    if bbox.x0 < 300 and bbox.width > 400:
+        left = round((310 - bbox.x0) * unit)
+        right = round((320 - bbox.x0) * unit)
+        vl = cv2.cvtColor(img[:,left:right], cv2.COLOR_BGR2GRAY)
+        if np.size(vl) == np.sum(vl==255):
+            img = img[:,:left]
+
+    if direction == 1:
+        img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+    return img
+
+
+def iter_docs(root):
+    for pdf in glob(f"{root}/papers/*.pdf"):
+        doi = os.path.basename(pdf)[:-4]
+        yield doi, pdf, f"{root}/output/{doi}/document.json"
+
+
+if __name__ == "__main__":
+    valid_table_pattern = r'^Table\s+([0-9]+)(?:\.|\s+Continued|$)'
+
+    root = os.path.expanduser("~/papers/z20230901")
+    for doi, pdf, meta in iter_docs(root):
+        doc, data = fitz.open(pdf), json.load(open(meta))
+        os.makedirs(f"images/{doi}", exist_ok=True)
+        for tbl in data["tables"]:
+            title, pn, bbox, direction = tbl["title"], tbl["page"], tbl["bbox"], tbl["direction"]
+            words, sentences = title.split(), title.split(".")
+            m = re.match(valid_table_pattern, title, re.I)
+            if m and (len(words) <= 50 or len(sentences) <= 3):
+                tix = m.group(1)
+                if title.lower().endswith('continued'):
+                    tix = tix + ".cont"
+                imgpath = f"images/{doi}/{doi}.table.{tix}.png"
+
+                bbox = fitz.Rect(bbox)
+                bbox.y1 = min(770, bbox.y1)
+                print(imgpath, '\t', title, bbox)
+                img = get_image(doc[pn], direction, bbox)
+                cv2.imwrite(imgpath, img)
